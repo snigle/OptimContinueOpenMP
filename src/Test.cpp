e00961196f83cf7f0b8e2df3ec61346802a23092
Ludovic Lamarche <ludovic.lamarche@eisti.fr>
2015-03-06 16:05:29 +0100

    Base projet CUTE

---
+++ src/Test.cpp
@@ -0,0 +1,26 @@
+#include "cute.h"
+#include "ide_listener.h"
+#include "xml_listener.h"
+#include "cute_runner.h"
+
+void init() {
+	ASSERTM("start writing tests",true);
+}
+
+void runAllTests(int argc, char const *argv[]){
+	cute::suite s{};
+
+	s.push_back(CUTE(init));
+
+	cute::xml_file_opener xmlfile(argc,argv);
+	cute::xml_listener<cute::ide_listener<> >  lis(xmlfile.out);
+	cute::makeRunner(lis,argc,argv)(s, "AllTests");
+}
+
+int main(int argc, char const *argv[]){
+    runAllTests(argc,argv);
+    return 0;
+}
+
+
+
