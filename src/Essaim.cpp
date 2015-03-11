e6934b561b521eb9557d88146cb55152c9036348
Ludovic Lamarche <ludovic.lamarche@eisti.fr>
2015-03-11 09:53:49 +0100

    Essaim fields + constructor

--- src/Essaim.cpp
+++ src/Essaim.cpp
@@ -6,21 +6,24 @@
  */
 
 #include "Essaim.h"
+using namespace std;
 
-Essaim::Essaim() {
-	// TODO Auto-generated constructor stub
+Essaim::Essaim(F_carre _obj, double _c1, double _c2, unsigned _nbParticules,
+		unsigned _cArret, vector<double> min, vector<double> max) :
+		obj(_obj), c1 { _c1 }, c2 { _c2 }, nbParticules { _nbParticules }, cArret {
+				_cArret }, particules(vector<vector<double>>(_nbParticules)), c(
+				vector<double>(_nbParticules)), xp(
+				vector<vector<double>>(_nbParticules)), cv(
+				vector<double>(_nbParticules)), xv(
+				vector<vector<double>>(_nbParticules)), v(
+				vector<double>(_nbParticules)),
+				borneMin(min), borneMax(max){
+
+
 
 }
 
-Essaim::Essaim(Objectif fonction):Algorithme(fonction)
-{
-
-}
-Essaim::~Essaim() {
-	// TODO Auto-generated destructor stub
-}
-
-int Essaim::solve()const{
+int Essaim::solve() const {
 
 }
 
--- src/Essaim.h
+++ src/Essaim.h
@@ -8,9 +8,18 @@
 #ifndef ESSAIM_H_
 #define ESSAIM_H_
 #include "Algorithme.h"
+#include "Fcarre.h"
 #include <vector>
-class Essaim:public Algorithme {
+class Essaim: public Algorithme {
 private:
+
+	F_carre obj;
+	double c1;
+	double c2;
+
+	unsigned nbParticules;
+	unsigned cArret;
+
 	std::vector<std::vector<double>> particules;
 	std::vector<double> c;
 	std::vector<std::vector<double>> xp;
@@ -18,16 +27,14 @@ private:
 	std::vector<std::vector<double>> xv;
 	std::vector<std::vector<double>> v;
 
-	double c1;
-	double c2;
-	Objectif obj;
-	unsigned nbParticules;
-	unsigned cArret;
+	std::vector<double> borneMin;
+	std::vector<double> borneMax;
 
 public:
-	Essaim(Objectif obj, double c1, double c2, unsigned nbParticules, unsigned cArret);
+	Essaim(F_carre _obj, double _c1, double _c2, unsigned _nbParticules,
+			unsigned _cArret, std::vector<double> min, std::vector<double> max);
 	virtual ~Essaim();
-	int solve()const;
+	int solve() const;
 };
 
 #endif /* ESSAIM_H_ */
