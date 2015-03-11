b11c45ae4fc15ed1c292902f7e6da9d95e180d3e
Ludovic Lamarche <ludovic.lamarche@eisti.fr>
2015-03-11 09:28:36 +0100

    essaim.h (private parameter)

--- src/Essaim.h
+++ src/Essaim.h
@@ -8,12 +8,24 @@
 #ifndef ESSAIM_H_
 #define ESSAIM_H_
 #include "Algorithme.h"
-
+#include <vector>
 class Essaim:public Algorithme {
-protected:
+private:
+	std::vector<std::vector<double>> particules;
+	std::vector<double> c;
+	std::vector<std::vector<double>> xp;
+	std::vector<double> cv;
+	std::vector<std::vector<double>> xv;
+	std::vector<std::vector<double>> v;
+
+	double c1;
+	double c2;
+	Objectif obj;
+	unsigned nbParticules;
+	unsigned cArret;
+
 public:
-	Essaim();
-	Essaim(Objectif);
+	Essaim(Objectif obj, double c1, double c2, unsigned nbParticules, unsigned cArret);
 	virtual ~Essaim();
 	int solve()const;
 };
