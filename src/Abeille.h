4bb2498ea324b34dccc821ad71d50778d0feca8f
Ludovic Lamarche <ludovic.lamarche@eisti.fr>
2015-03-27 09:42:18 +0100

    constructeur warning et solve

--- src/Abeille.h
+++ src/Abeille.h
@@ -27,12 +27,12 @@ private:
     unsigned nbFleurs;
 
     std::default_random_engine generator;
-    std::uniform_real_distribution<double> distribution/*(0, 1)*/;
+    std::uniform_int_distribution<int> distribution/*(0, 1)*/;
     std::vector<std::uniform_real_distribution<double>> distributionParticule/*(dimension)*/;
 
     unsigned dimension;
 
-	double fitness(std::vector<double>)const;
+    double fitness(std::vector<double>)const;
     void initVectors();
     void majFitnesses();
     std::vector<double> getVoisin(unsigned i);
@@ -43,6 +43,29 @@ public:
     void solve();
     void testInitFleurs()const;
 };
+
+template<typename F>
+Abeille<F>::Abeille(F _obj, unsigned _nbFleurs, unsigned _max) :
+pFleurs(new std::vector<std::vector<double> >(_nbFleurs))
+, pFitnesses(new std::vector<double>(_nbFleurs))
+, pIterations(new std::vector<unsigned>(_nbFleurs))
+, obj(_obj)
+, maxIterations(_max)
+, nbFleurs(_nbFleurs)
+, generator()
+, distribution(0, _nbFleurs)
+, distributionParticule(_obj.getMax().size())
+, dimension(_obj.getMax().size()) {
+
+    for (unsigned i = 0; i < dimension; ++i) {
+        distributionParticule[i] = std::uniform_real_distribution<double>(obj.getMin()[i], obj.getMax()[i]);
+    }
+    initVectors();
+
+    //	fleurs=*pFleurs;
+    //	fitnesses=*pFitnesses;
+    //	iterations=*pIterations;
+}
 
 template <typename F>
 std::vector<double> Abeille<F>::genererFleur() {
@@ -75,26 +98,26 @@ void Abeille<F>::majFitnesses() {
 
 template<typename F>
 double Abeille<F>::fitness(std::vector<double> fleur) const {
-	double valFleur = obj.f(fleur);
+    double valFleur = obj.f(fleur);
 
-	if(valFleur >= 0) return (1/(valFleur+1));
-	else return (1+fabs(valFleur));
+    if (valFleur >= 0) return (1 / (valFleur + 1));
+    else return (1 + fabs(valFleur));
 
 }
 
 template<typename F>
 std::vector<double> Abeille<F>::getVoisin(unsigned i) {
-	std::vector<std::vector<double>> &fleurs = *pFleurs;
+    std::vector<std::vector<double>> &fleurs = *pFleurs;
 
-	unsigned aleaDimension;
+    unsigned aleaDimension;
 
-	std::vector<double> fleur = genererFleur();
-	std::vector<double> voisine = fleurs[i];
-	aleaDimension = (unsigned) (rand()/(double)RAND_MAX)*dimension;
+    std::vector<double> fleur = genererFleur();
+    std::vector<double> voisine = fleurs[i];
+    aleaDimension = (unsigned) (rand() / (double) RAND_MAX) * dimension;
 
-	voisine[aleaDimension] = voisine[aleaDimension]
-					 + ((rand()/(double)RAND_MAX)*2-1)
-					 * ( voisine[aleaDimension] - fleur[aleaDimension] );
+    voisine[aleaDimension] = voisine[aleaDimension]
+            + ((rand() / (double) RAND_MAX)*2 - 1)
+            * (voisine[aleaDimension] - fleur[aleaDimension]);
 
 
     return 0.0;
@@ -127,40 +150,28 @@ void Abeille<F>::initVectors() {
     //Initialisation des fleurs.
 
     for (unsigned i = 0; i < nbFleurs; ++i) {
-       fleurs[i] = genererFleur();
+        fleurs[i] = genererFleur();
     }
-}
-
-template<typename F>
-Abeille<F>::Abeille(F _obj, unsigned _nbFleurs, unsigned _max) :
-pFleurs(new std::vector<std::vector<double> >{_nbFleurs})
-, pFitnesses(new std::vector<double>{_nbFleurs})
-, pIterations(new std::vector<unsigned>{_nbFleurs}), obj(_obj)
-, maxIterations {
-    _max
-}
-, nbFleurs(_nbFleurs)
-
-, generator()
-, distribution(0, 1)
-, distributionParticule(_obj.getMax().size())
-, dimension (
-     _obj.getMax().size()
-)
-{
-
-    for (unsigned i = 0; i < dimension; ++i) {
-        distributionParticule[i] = std::uniform_real_distribution<double>(obj.getMin()[i], obj.getMax()[i]);
-    }
-    initVectors();
-
-    //	fleurs=*pFleurs;
-    //	fitnesses=*pFitnesses;
-    //	iterations=*pIterations;
 }
 
 template<typename F>
 void Abeille<F>::solve() {
+    std::vector<double>& fitnesses = *pFitnesses;
+    std::vector<std::vector<double>>&fleurs = *pFleurs;
+    std::vector<unsigned>& iterations = *pIterations;
+
+    for (unsigned i = 0; i < 10000; ++i) {
+        majFitnesses();
+        for (unsigned scout = 0; scout < nbFleurs; ++scout) {
+            unsigned position = distribution(generator);
+            std::vector<double>& fleur = fleurs[position];
+            std::vector<double> voisin = genererFleur();
+            if (fitnesses[position] < fitness(voisin)) {
+                fleur = voisin;
+                iterations[position] = 0;
+            }
+        }
+    }
 }
 
 template<typename F>
